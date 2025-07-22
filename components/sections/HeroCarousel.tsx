90da0bfeec1f097d13910a1f0c2a4bd2452e2d90
Christopher Flodin <christopher.flodin@gmail.com>
2025-07-22 14:00:40 +0200

    fix carousel bug

--- components/sections/HeroCarousel.tsx
+++ components/sections/HeroCarousel.tsx
@@ -86,7 +86,7 @@ export default function HeroCarousel({ projects }: HeroCarouselProps) {
             skipSnaps: false,
             dragFree: false,
           }}
-          className={`w-full h-full transition-all duration-700 ease-out ${
+          className={`w-full h-full ${
             isCarouselReady
               ? "opacity-100 translate-y-0 scale-100"
               : "opacity-0 translate-y-4 scale-95"
